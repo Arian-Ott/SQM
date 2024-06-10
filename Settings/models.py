dde7680db73b94476fccf8e835c25f741ac35f75
Arian <88984301+Arian-Ott@users.noreply.github.com>
2024-06-10 23:01:55 +0200

    FIXED Security issue with logging Fernet Keys
    Signed-off-by: Arian <88984301+Arian-Ott@users.noreply.github.com>

--- Settings/models.py
+++ Settings/models.py
@@ -1,14 +1,27 @@
-import base64
+
+#   <one line to give the program's name and a brief idea of what it does.>
+#      Copyright (c) 2024,.  Arian Ott
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import uuid
-from datetime import datetime, timedelta, timezone
 
+from cryptography.fernet import Fernet, InvalidToken
 from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
 from django.db import models
-from cryptography.fernet import Fernet, InvalidToken
-from hashlib import sha512, pbkdf2_hmac
-import mariadb
 
 
 # Create your models here.
@@ -39,7 +52,6 @@ class DB(models.Model):
         #  password=sha512(self.root_pw.encode(), usedforsecurity=True).hexdigest().encode(),
         # iterations=700000, dklen=127).hex()
 
-        print(self.secret_key, self.salt, self.root_pw)
         super().save(*args, **kwargs)
 
     def get_root_pw(self, owner):
