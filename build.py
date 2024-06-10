7e7b5fd7e76cb63bff41b1f1d084871b30cbf4fd
Arian <88984301+Arian-Ott@users.noreply.github.com>
2024-06-10 08:59:51 +0200

    Changed All files
    Signed-off-by: Arian <88984301+Arian-Ott@users.noreply.github.com>

---
+++ build.py
@@ -0,0 +1,43 @@
+#   <one line to give the program's name and a brief idea of what it does.>
+#      Copyright (c) 2024,.  Arian Ott
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# importing required packages
+import os
+
+# defining source and destination
+# paths
+src = '.'
+trg = '.dev'
+if os.path.exists(trg):
+    os.system("rm -rf .dev")
+os.scandir(".")
+os.system("cp -r . .dev")
+os.system("cd .dev && rm -rf venv")
+os.system("rm -rf .dev")
+exit(0)
